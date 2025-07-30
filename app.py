26cb1c0791fe3335105e9079852383ae74bb9f2c
RichardButterworth1 <rbutterworth@enhesa.com>
2025-07-31 01:10:27 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -39,8 +39,8 @@ def add_to_cadence():
 
         custom_fields_payload = {
             "custom email template": memo,
-            "Owner_CRM_ID": "ad9a474d-36c4-eb11-bacc-002248801824",
-            "Account_CRM_ID": "ac663391-cd49-eb11-bb23-000d3a219bd5"
+            "Owner_CRM_ID": "Richard Butterworth",
+            "Account_CRM_ID": "5397792"
         }
 
         if people_data:
