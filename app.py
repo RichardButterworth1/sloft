9b8bd90fe58a98cd20a94e093c7f5413e9bdf42d
RichardButterworth1 <rbutterworth@enhesa.com>
2025-07-30 17:46:51 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -66,7 +66,6 @@ return jsonify({
     "success": True,
     "message": f"{first_name} {last_name} successfully added to cadence '{cadence_name}'."
 })
-```
 
-if **name** == '**main**':
+if __name__ == '__main__':
 app.run(host='0.0.0.0', port=int(os.getenv("PORT", 10000)))
